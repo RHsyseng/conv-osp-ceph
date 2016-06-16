abe4592b7359fe7e85a1a98fd55fee31937632c7
John Fulton <fulton@redhat.com>
2016-06-15 20:34:53 -0400

    Update: nova_mem_cpu_calc.py. Adding Ben's addition which takes the guest CPU utilization into account

--- scripts/nova_mem_cpu_calc.py
+++ scripts/nova_mem_cpu_calc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # Filename:                nova_mem_cpu_calc.py
 # Supported Langauge(s):   Python 2.7.x
-# Time-stamp:              <2016-06-15 09:20:29 jfulton> 
+# Time-stamp:              <2016-06-15 20:11:35 jfulton> 
 # -------------------------------------------------------
 # This program was originally written by Ben England
 # -------------------------------------------------------
@@ -11,8 +11,9 @@
 # input command line parameters:
 # 1 - total host RAM in GB
 # 2 - total host cores 
-# 3 - average guest size in GB
-# 4 - Ceph OSDs per server
+# 3 - Ceph OSDs per server
+# 4 - average guest size in GB
+# 5 - average guest CPU utilization (0.0 to 1.0)
 #
 # It assumes that we want to allow 3 GB per OSD 
 # (based on prior Ceph Hammer testing)
@@ -29,18 +30,22 @@ MB_per_GB = 1000
 
 GB_per_OSD = 3
 GB_overhead_per_guest = 0.5  # based on measurement in test environment
+cores_per_OSD = 1.0  # may be a little low in I/O intensive workloads
 
 def usage(msg):
   print msg
-  print "Usage: %s Total-host-RAM-GB Total-host-cores Avg-guest-size-GB OSDs-per-server" % sys.argv[0]
+  print(
+    ("Usage: %s Total-host-RAM-GB Total-host-cores OSDs-per-server " + 
+     "Avg-guest-size-GB Avg-guest-CPU-util") % sys.argv[0])
   sys.exit(NOTOK)
 
 if len(argv) < 5: usage("Too few command line params")
 try:
   mem = int(argv[1])
   cores = int(argv[2])
-  average_guest_size = int(argv[3])
-  osds = int(argv[4])
+  osds = int(argv[3])
+  average_guest_size = int(argv[4])
+  average_guest_util = float(argv[5])
 except ValueError:
   usage("Non-integer input parameter")
 
@@ -48,8 +53,9 @@ except ValueError:
 print "Inputs:"
 print "- Total host RAM in GB: %d" % mem
 print "- Total host cores: %d" % cores
-print "- Average guest size in GB: %d" % average_guest_size
 print "- Ceph OSDs per host: %d" % osds
+print "- Average guest size in GB: %d" % average_guest_size
+print "- Average guest utilization: %f" % average_guest_util
 
 # calculate operating parameters based on memory constraints only
 left_over_mem = mem - (GB_per_OSD * osds)
@@ -57,15 +63,23 @@ number_of_guests = int(left_over_mem /
                        (average_guest_size + GB_overhead_per_guest))
 nova_reserved_mem_MB = MB_per_GB * int(left_over_mem - 
                        (number_of_guests * GB_overhead_per_guest))
-cpu_allocation_ratio = float((cores - osds)) / float(cores)
+nonceph_cores = cores - (cores_per_OSD * osds)
+guest_vCPUs = nonceph_cores / average_guest_util
+cpu_allocation_ratio = guest_vCPUs / cores
 
 # display outputs including how to tune Nova reserved mem
 
 print "\nResults:"
 print "- number of guests allowed = %d" % number_of_guests
+print "- number of guest vCPUs allowed = %d" % int(guest_vCPUs)
 print "- nova.conf reserved_host_memory = %d MB" % nova_reserved_mem_MB
 print "- nova.conf cpu_allocation_ratio = %f" % cpu_allocation_ratio
 
 if nova_reserved_mem_MB < 0:
     print "ERROR: you do not have enough memory to run hyperconverged!"
     sys.exit(NOTOK)
+
+if cpu_allocation_ratio < 0.0:
+    print "ERROR: you do not have enough CPU to run hyperconverged!"
+    sys.exit(NOTOK)
+
